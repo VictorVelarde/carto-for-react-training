c0ea565759ee047eade3e6e49eb36adf5473ba81
VictorVelarde <victor.velarde@gmail.com>
2021-01-17 21:18:39 +0100

    new home tab - basic dispatch
    A dispatch to change the view

--- setup/my-app/src/components/views/Home.js
+++ setup/my-app/src/components/views/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { useDispatch } from 'react-redux';
+import { setViewState } from '@carto/react/redux';
+
 import { Button, makeStyles, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
@@ -11,14 +14,19 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Home() {
+  const dispatch = useDispatch();
   const classes = useStyles();
+
+  const goHome = () => {
+    dispatch(setViewState({ longitude: -3.8124194, latitude: 43.451641, zoom: 11 }));
+  };
 
   return (
     <div className={classes.root}>
       <Typography variant='h5' gutterBottom>
         Home
       </Typography>
-      <Button variant='contained' color='primary'>
+      <Button variant='contained' color='primary' onClick={goHome}>
         Go Home
       </Button>
     </div>
