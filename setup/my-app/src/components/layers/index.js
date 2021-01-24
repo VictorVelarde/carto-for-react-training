331763990270e3739dfaffde0c1273bff10da1ed
VictorVelarde <victor.velarde@gmail.com>
2021-01-24 11:55:38 +0100

    add: NewLayer with hygen
    An SQL layer, displayed on NewView

--- setup/my-app/src/components/layers/index.js
+++ setup/my-app/src/components/layers/index.js
@@ -1,11 +1,13 @@
 import GeocoderLayer from './GeocoderLayer';
 import DynamicLayer from './DynamicLayer';
+import NewLayer from './NewLayer';
 // Auto import
 
 export const getLayers = () => {
   return [
     GeocoderLayer(),
     DynamicLayer(),
+    NewLayer(),
     // Auto import layers
   ];
 };
