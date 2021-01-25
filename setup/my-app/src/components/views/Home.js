60a6ce26234153c246dc18e20cea134d39cbf796
VictorVelarde <victor.velarde@gmail.com>
2021-01-25 17:04:03 +0100

    feat: add basemap selector

--- setup/my-app/src/components/views/Home.js
+++ setup/my-app/src/components/views/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { setViewState } from '@carto/react/redux';
+import { setViewState, setBasemap } from '@carto/react/redux';
+import { POSITRON, VOYAGER, DARK_MATTER, GOOGLE_ROADMAP, GOOGLE_SATELLITE, GOOGLE_HYBRID } from '@carto/react/basemaps';
 
-import { Button, makeStyles, TextField, Typography } from '@material-ui/core';
+import { Button, FormControl, InputLabel, makeStyles, MenuItem, Select, TextField, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,12 +26,39 @@ export default function Home() {
     dispatch(setViewState({ longitude: -3.8124194, latitude: 43.451641, zoom: 11 }));
   };
 
+
+  const basemap = useSelector((state) => state.carto.basemap);
+  const handleBasemapChange = (event) => {
+    debugger;
+    const selected = event.target.value;
+    dispatch(setBasemap(selected));
+  }
+
   return (
     <div className={classes.root}>
       <Typography variant='h5' gutterBottom>
         Home
       </Typography>
 
+      {/* Basemap selector */}
+      <FormControl>
+        <InputLabel id="basemapSelectorLabel">Basemap</InputLabel>
+        <Select
+          labelId="basemapSelectorLabel"
+          id="basemapSelector"
+          value={basemap}
+          onChange={handleBasemapChange}
+        >
+          <MenuItem value={POSITRON}>Positron</MenuItem>
+          <MenuItem value={VOYAGER}>Voyager</MenuItem>
+          <MenuItem value={DARK_MATTER}>Dark matter</MenuItem>
+          <MenuItem value={GOOGLE_ROADMAP}>G. Roadmap</MenuItem>
+          <MenuItem value={GOOGLE_SATELLITE}>G. Satellite</MenuItem>
+          <MenuItem value={GOOGLE_HYBRID}>G. Hybrid</MenuItem>
+        </Select>
+      </FormControl>
+
+      {/* Center coordinates */}
       <TextField
         label='Longitude'
         value={viewState.longitude}
@@ -50,6 +78,7 @@ export default function Home() {
       <Button variant='contained' color='primary' onClick={goHome}>
         Go Home
       </Button>
+
     </div>
   );
 }
